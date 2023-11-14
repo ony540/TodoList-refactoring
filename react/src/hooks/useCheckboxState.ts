76df3c9b51a3f9a697039e51bd4263d30929aa3b
jingoworld <lonul83@gmail.com>
2023-11-14 09:56:47 +0900

    feat: 체크박스 중복 클릭 방지 구현
    - 0.5초에 한 번 클릭 이벤트 가능하게 구현

--- react/src/hooks/useCheckboxState.ts
+++ react/src/hooks/useCheckboxState.ts
@@ -1,24 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { updateChecked } from '@/api/TodoAPI';
 
 export const useCheckboxState = initialTodo => {
   const [isChecked, setIsChecked] = useState(initialTodo.done);
+  const throttleTimer = useRef<null | NodeJS.Timeout>(null);
 
   useEffect(() => {
     setIsChecked(initialTodo.done);
   }, [initialTodo.done]);
 
-  const handleCheckboxChange = async () => {
-    if (initialTodo._id !== undefined) {
-      try {
-        const updatedStatus = !isChecked;
-        await updateChecked(initialTodo._id, initialTodo.title, initialTodo.content, updatedStatus);
-        setIsChecked(updatedStatus);
-      } catch (error) {
-        console.error('Error updating checked state:', error);
+  const handleCheckboxChange = () => {
+    if (!throttleTimer.current) {
+      // 타이머가 없으면 즉시 실행하고 타이머 설정
+      throttleTimer.current = setTimeout(() => {
+        throttleTimer.current = null;
+      }, 500);
+
+      if (initialTodo._id !== undefined) {
+        try {
+          const updatedStatus = !isChecked;
+          updateChecked(initialTodo._id, initialTodo.title, initialTodo.content, updatedStatus).then(() =>
+            setIsChecked(updatedStatus),
+          );
+        } catch (error) {
+          console.error('Error updating checked state:', error);
+        }
       }
     }
   };
+
+  useEffect(() => {
+    // 컴포넌트 언마운트 시 타이머 취소
+    return () => {
+      if (throttleTimer.current) {
+        clearTimeout(throttleTimer.current);
+      }
+    };
+  }, []);
 
   return { isChecked, handleCheckboxChange };
 };
